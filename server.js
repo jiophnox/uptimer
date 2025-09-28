c85807302e295137d32489dfb3592a5d6f9c9ec4
jiophnox <jiophnox@gmail.com>
2025-09-28 13:56:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const URLs = [
   "https://hivajoy.onrender.com",
   "https://hjuptimer.onrender.com",
-  "https://hivajoy-file-to-link-bot.onrender.com",
+  "https://hivajoy-file-to-link-bot-vats.onrender.com",
   "https://terabox-play.onrender.com",
   "https://og-terabox-player.onrender.com",
   "https://astrologerbot.onrender.com"
